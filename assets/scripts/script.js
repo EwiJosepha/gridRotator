ff9057b6d1ff88e1bff124cc8f8ce41432a371f4
ewi josepha <ewijosepha@gmail.com>
2023-09-08 12:54:27 +0200

    adding functionality to my rotator

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,12 +6,12 @@ const one = document.getElementById('one')
 const two = document.getElementById('two')
 const three = document.getElementById('three')
 const four = document.getElementById('four')
-const five = document.getElementById('five')
+// const five = document.getElementById('five')
 const six = document.getElementById('six')
 const seven = document.getElementById('seven')
 const eight = document.getElementById('eight')
 const nine = document.getElementById('nine')
-  
+
 function clockwise (numbers) {
   numbers.unshift(numbers.pop())
 }
@@ -21,52 +21,51 @@ function anticlock (numbers) {
 }
 
 function update (numbers) {
-one.innerHTML = numbers[0]
-two.innerHTML = numbers[1]
-three.innerHTML = numbers[2]
-four.innerHTML = numbers[3]
-six.innerHTML = numbers[4]
-seven.innerHTML = numbers[5]
-eight.innerHTML = numbers[6]
-nine.innerHTML = numbers[7]
+  one.innerHTML = numbers[0]
+  two.innerHTML = numbers[1]
+  three.innerHTML = numbers[2]
+  four.innerHTML = numbers[3]
+  six.innerHTML = numbers[4]
+  seven.innerHTML = numbers[5]
+  eight.innerHTML = numbers[6]
+  nine.innerHTML = numbers[7]
 }
 
 rotater.addEventListener('click', ()=> {
-clockwise(numbers)
-update(numbers)
+ clockwise(numbers)
+ update(numbers)
 })
 
 one.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 seven.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 two.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 three.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 four.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 
 six.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 eight.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
 nine.addEventListener('click', () => {
-anticlock(numbers)
-update(numbers)
+  anticlock(numbers)
+  update(numbers)
 })
-  
